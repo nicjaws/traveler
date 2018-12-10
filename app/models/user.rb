5835d3c91cd47cbe744f571ca26ef40cb309ac2c
Nicolas Corbalan Calvo <nicolascc@administrators-iMac-4.local>
2018-12-10 15:33:41 +0100

    adding facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :omniauthable
 
-         validates :fullname, presence: true, length: {maximum: 50
+         validates :fullname, presence: true, length: {maximum: 50}
           
           def self.from_omniauth(auth)
             user = User.where(email: auth.info.email).first
